e3fd5ec2e06071055c15b9a496689c721942f111
blaec <WVm8opYk5}>
2020-02-04 21:58:26 +0200

    74. CSS Modules & Media Queries

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
 
     render() {
         let persons = null;
-        let btnClass = [classes.Button];
+        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
@@ -72,7 +72,7 @@ class App extends Component {
                 </div>
             );
 
-            btnClass.push(classes.Red);
+            btnClass = classes.Red;
         }
 
         let assignedClasses = [];
@@ -87,7 +87,7 @@ class App extends Component {
             <div className={classes.App}>
                 <h1>Hi, I'm a React App</h1>
                 <p className={assignedClasses.join(' ')}>This is really working!</p>
-                <button className={btnClass.join(' ')}
+                <button className={btnClass}
                         onClick={this.togglePersonsHandler}>
                     Toggle Persons
                 </button>
