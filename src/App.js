68269847c50f3233c30acd08180eaeb2c86b3c6f
blaec <WVm8opYk5}>
2020-02-04 21:51:50 +0200

    73.4 Working with CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -56,20 +56,8 @@ class App extends Component {
     };
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1x solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
-        };
-
         let persons = null;
+        let btnClass = [classes.Button];
 
         if (this.state.showPersons) {
             persons = (
@@ -84,26 +72,22 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            };
+            btnClass.push(classes.Red);
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
-                <button className="button"
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
+                <button className={btnClass.join(' ')}
                         onClick={this.togglePersonsHandler}>
                     Toggle Persons
                 </button>
