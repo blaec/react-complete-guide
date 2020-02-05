21831f04b360f1fd87cac4d71e024d81270695f8
blaec <WVm8opYk5}>
2020-02-05 21:19:31 +0200

    81. Using Error Boundaries (React 16+)

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,11 +2,10 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
+    const rnd = Math.random();
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
     return (
         // <div className="Person" style={style}>
         <div className={classes.Person}>
