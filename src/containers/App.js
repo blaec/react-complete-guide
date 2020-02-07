deb253631182cabf4b58876de8001df3b59f5b51
blaec <WVm8opYk5}>
2020-02-07 14:18:37 +0200

    98. Optimizing Functional Components with React.memo()

--- src/containers/App.js
+++ src/containers/App.js
@@ -104,7 +104,7 @@ class App extends Component {
                 </button>
                 {this.state.showCockpit ? (<Cockpit title={this.props.appTitle}
                                                     showPersons={this.state.showPersons}
-                                                    persons={this.state.persons}
+                                                    personsLength={this.state.persons.length}
                                                     clicked={this.togglePersonsHandler}/>
                 ) : null}
                 {persons}
