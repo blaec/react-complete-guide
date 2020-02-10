ef356b32ad89812cd2c00e2430d76f131978ab3d
blaec <WVm8opYk5}>
2020-02-10 21:40:48 +0200

    110.1 Using Refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,10 @@ import classes from './Person.css';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+    componentDidMount() {
+        this.inputElement.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -12,6 +16,7 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text"
+                       ref={(inputEl) => {this.inputElement = inputEl}}
                        onChange={this.props.changed}
                        value={this.props.name}/>
             </Aux>
