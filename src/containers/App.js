d1efb007ab8bf4ed6e7171183c14c800466c8626
blaec <WVm8opYk5}>
2020-02-13 00:02:08 +0200

    122. [LEGACY] Component Creation Lifecycle in Action

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,19 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[App.js] inside constructor', props);
+    }
+
+    componentWillMount() {
+        console.log('[App.js] inside componentWillMount()');
+    }
+
+    componentDidMount() {
+        console.log('[App.js] inside componentDidMount()');
+    }
+
     state = {
         persons: [
             {id: 'a1', name: 'Max', age: 28},
@@ -58,6 +71,7 @@ class App extends Component {
     };
 
     render() {
+        console.log('[App.js] render()');
         let persons = null;
 
         if (this.state.showPersons) {
