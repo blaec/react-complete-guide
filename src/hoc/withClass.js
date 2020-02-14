a67022c64f7b4679739cdfd3023678d75a600119
blaec <WVm8opYk5}>
2020-02-14 12:24:07 +0200

    135.2 [LEGACY] Passing Unknown Props

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 const withClass = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent {...props}/>
-        </div>
-    );
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props}/>
+                </div>
+            );
+        }
+    }
+    // return (props) => (
+    //     <div className={className}>
+    //         <WrappedComponent {...props}/>
+    //     </div>
+    // );
 };
 
 export default withClass;
