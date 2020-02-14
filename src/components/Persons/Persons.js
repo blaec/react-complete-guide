5623d7cc7b56e1875df8eb80694a9f34c3de2861
blaec <WVm8opYk5}>
2020-02-14 11:35:57 +0200

    127.2 [LEGACY] Performance Gains with PureComponents

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
     constructor(props) {
         super(props);
         console.log('[Persons.js] inside constructor', props);
@@ -19,13 +19,13 @@ class Persons extends Component {
         console.log('[UPDATE Persons.js] inside componentWillReceiveProps()', nextProps);
     }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        console.log('[UPDATE Persons.js] inside shouldComponentUpdate()', nextProps, nextState);
-        return nextProps.persons !== this.props.persons ||
-            nextProps.changed !== this.props.changed ||
-            nextProps.clicked !== this.props.clicked;
-        // return true;
-    }
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     console.log('[UPDATE Persons.js] inside shouldComponentUpdate()', nextProps, nextState);
+    //     return nextProps.persons !== this.props.persons ||
+    //         nextProps.changed !== this.props.changed ||
+    //         nextProps.clicked !== this.props.clicked;
+    //     // return true;
+    // }
 
     componentWillUpdate(nextProps, nextState, nextContext) {
         console.log('[UPDATE Persons.js] inside componentWillUpdate()', nextProps, nextState);
