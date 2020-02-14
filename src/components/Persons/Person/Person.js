ace26a7f4761db5f725bb65e07e1395457db36d9
blaec <WVm8opYk5}>
2020-02-14 11:51:05 +0200

    130.1 [LEGACY] Windows Users Must Read - File Downloads

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -21,16 +21,23 @@ class Person extends Component {
 
     render () {
         console.log('[Person.js] inside render()');
-        return (
-            // <div className="Person" style={style}>
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text"
+        return [
+                <p id="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+                <p id="2">{this.props.children}</p>,
+                <input id="3" type="text"
                        onChange={this.props.changed}
                        value={this.props.name}/>
-            </div>
-        )
+        ]
+        // return (
+        //     // <div className="Person" style={style}>
+        //     <div className={classes.Person}>
+        //         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        //         <p>{this.props.children}</p>
+        //         <input type="text"
+        //                onChange={this.props.changed}
+        //                value={this.props.name}/>
+        //     </div>
+        // )
     }
 }
 
